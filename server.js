cd58226c184b5ca6fac49871cb9d04b92311ada3
Aakash bhardwaj <54072018+Aakashbhardwaj27@users.noreply.github.com>
2020-06-20 19:30:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const app = express()
 const path = require('path');
 const port = 3000
 
-app.get('/', (req, res) => res.sendfile(path.join(__dirname + '/frontend/home.html')));
-app.get('/dashboard.html', (req, res) => res.sendfile(path.join(__dirname + '/frontend/dashboard.html')));
-app.get('/home.html', (req, res) => res.sendfile(path.join(__dirname + '/frontend/home.html')));
-app.get('/login.html', (req, res) => res.sendfile(path.join(__dirname + '/frontend/login.html')));
-app.get('/signup.html', (req, res) => res.sendfile(path.join(__dirname + '/frontend/signup.html')));
+app.get('/', (req, res) => res.sendfile(path.join(__dirname + '/home.html')));
+app.get('/dashboard.html', (req, res) => res.sendfile(path.join(__dirname + '/dashboard.html')));
+app.get('/home.html', (req, res) => res.sendfile(path.join(__dirname + '/home.html')));
+app.get('/login.html', (req, res) => res.sendfile(path.join(__dirname + '/login.html')));
+app.get('/signup.html', (req, res) => res.sendfile(path.join(__dirname + '/signup.html')));
 
 
 
